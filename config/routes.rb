3926f664adbfe0dd85c89c891a5fc544a4d72033
kozkoz-shio <enkoz15@gmail.com>
2020-04-06 00:46:30 +0900

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   root 'top#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update,:show]
   resources :maigos
   
 end
