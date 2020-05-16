9406f58beeb90bbe644a5267dd005aee98448171
kozkoz-shio <enkoz15@gmail.com>
2020-05-16 22:51:07 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,3 +28,22 @@ set :keep_releases, 5
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 
 set :linked_files, %w{ config/secrets.yml }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
